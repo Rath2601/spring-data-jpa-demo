c4569ce1e3e6eab9e6ac32a1507a2eaccd601356
Rath2601 <131427196+Rath2601@users.noreply.github.com>
2023-11-27 19:42:54 +0400

    Add files via upload

---
+++ SpringDataJPA/springdata-jpa-demo/src/main/java/com/spring/data/SpringDataMainApplication.java
@@ -0,0 +1,17 @@
+package com.spring.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringDataMainApplication {
+
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMainApplication.class, args);
+	}
+
+}
---
+++ SpringDataJPA/springdata-jpa-demo/src/main/java/com/spring/data/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.spring.data.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+
+}
